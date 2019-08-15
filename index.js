5fe47c711a25e1ac15df7c42440427ea134066e5
Kingsley Oha <KingsleyOha@Names-MacBook-Air.local>
2019-08-15 19:36:05 +0200

    a little modification

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'I am Kingsley' });
+  res.send({ hi: 'I am Kingsley, I like you' });
 });
 
 const PORT = process.env.PORT || 5000;
