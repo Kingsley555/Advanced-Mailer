00669339e79b8ad38720416a82a87ebd5018c9b9
Kingsley Oha <KingsleyOha@Names-MacBook-Air.local>
2019-08-18 21:04:00 +0200

    assigned literals to prod js variables

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 module.exports = {
-  googleClientID: `process.env.GOOGLE_CLIENT_ID`,
-  googleClientSecret: `process.env.GOOGLE_CLIENT_SECRET`,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   // facebookClientID: '',
   // facebookClientSecret: '',
-  mongoURI: `process.env.MONGO_URI`,
-  cookieKey: `process.env.COOKIE_KEY`
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
